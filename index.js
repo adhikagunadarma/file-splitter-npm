dd971021df13a1548145f55579d6227b122ab15f
Bizzy Indonesia <bizzyindonesia@Bizzys-MacBook-Pro.local>
2021-09-13 13:09:35 +0700

    implements fragment splitter for npm packages

---
+++ index.js
@@ -0,0 +1,29 @@
+const fragmentSplitter = (file, fragmentSize) => {
+
+    var fragmentSize = fragmentSize * 1024 * 1024; // coba dari per 100 kb, nanti harusnya dari input yg dikasih
+    var fileSize = file.size;
+    var totalFragments = Math.ceil(fileSize/fragmentSize,fragmentSize);
+    var fragmentTracker = 0;
+    var fragments = []
+  
+    console.log('file size = ',fileSize);
+    console.log('total fragment = ',totalFragments);
+  
+    while (fragmentTracker < totalFragments) {
+        var offset = fragmentTracker*fragmentSize;
+        console.log('current fragment..', fragmentTracker);
+        console.log('offset...', fragmentTracker*fragmentSize);
+        console.log('file blob from offset...', offset)
+        console.log(file.slice(offset,offset+fragmentSize));
+        fragments.push({
+          sequenceNumber : fragmentTracker,
+          sequenceFile : uploadFile.slice(offset,offset+fragmentSize)
+        })
+        fragmentTracker++;
+    }
+    // console.log(fragments)
+    return fragments
+  }
+
+
+  exports.module.fragmentSplitter = fragmentSplitter
